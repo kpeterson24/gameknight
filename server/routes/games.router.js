0f281c4f9e436cb784c45005cc8dd7a9a6f87b65
Korey Peterson <korey.t.peterson@gmail.com>
2020-01-16 07:48:11 -0600

    game route setup

--- server/routes/games.router.js
+++ server/routes/games.router.js
@@ -6,7 +6,13 @@ const router = express.Router();
  * GET route template
  */
 router.get('/', (req, res) => {
-    
+    let queryText = `SELECT * FROM "games";`
+    pool.query(queryText).then( result => {
+        res.send(result.rows);
+    }).catch( error => {
+        console.log('error getting games', error);
+        res.sendStatus(500);
+    }); 
 });
 
 /**

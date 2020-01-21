944e78cd3f2ec500aac6d4629e3e2f669ee251cb
Korey Peterson <korey.t.peterson@gmail.com>
2020-01-21 17:10:48 -0600

    adding friends and emails in form works

--- src/components/AddEvent/AddEvent.js
+++ src/components/AddEvent/AddEvent.js
@@ -9,7 +9,12 @@ import Button from '@material-ui/core/Button';
 import SaveIcon from '@material-ui/icons/Save';
 // import Grid from '@material-ui/core/Grid';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import { InputAdornment } from '@material-ui/core';
+import { InputAdornment, InputLabel, Select } from '@material-ui/core';
+import MenuItem from '@material-ui/core/MenuItem';
+import FormControl from '@material-ui/core/FormControl';
+import Chip from '@material-ui/core/Chip';
+import Input from '@material-ui/core/Input';
+
 
 // Material Ui styles brought in for the text fields.
 const styles = theme => ({
@@ -22,13 +27,35 @@ const styles = theme => ({
         padding: '0 30px',
         boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
       },
+      
+      multipleRoot: {
+        display: 'flex',
+        flexWrap: 'wrap',
+      },
+      formControl: {
+        margin: theme.spacing.unit,
+        minWidth: 120,
+        maxWidth: 300,
+      },
+      chips: {
+        display: 'flex',
+        flexWrap: 'wrap',
+      },
+      chip: {
+        margin: theme.spacing.unit / 4,
+      },
+      noLabel: {
+        marginTop: theme.spacing.unit * 3,
+      },
 
-    container: {
+     container: {
       display: 'flex',
       flexWrap: 'wrap',
-      height: '475px',
+      height: 'relative',
+      width: '100%',
     },
     textField: {
+      width: '75%',
       marginLeft: theme.spacing.unit,
       marginRight: theme.spacing.unit,
     },
@@ -52,6 +79,33 @@ const styles = theme => ({
       }
   });
 
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+    PaperProps: {
+        style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+        },
+    },
+};
+
+const names = [
+    'Korey',
+    'Jamie',
+    'David',
+    'Elmi', 
+];
+
+function getStyles(name, that) {
+    const weight = that.props.theme.typography;
+    return {
+        
+        fontWeight:
+            that.state.name.indexOf(name) === -1 ? weight.fontWeightRegular : weight.fontWeightMedium,
+    };
+}
+
   
 class AddEvent extends Component {
 
@@ -59,7 +113,8 @@ class AddEvent extends Component {
         title:'',
         location:'',
         description:'',
-        date: ''
+        date: '',
+        name: [],
       };
     
       handleChange = (event, propName) => {
@@ -68,9 +123,26 @@ class AddEvent extends Component {
         });
       };
 
+      handleChange = event => {
+          this.setState({ name: event.target.value });
+      };
+
+      handleChangeMultiple = event => {
+        const { options } = event.target;
+        const value = [];
+        for (let i = 0, l = options.length; i < l; i += 1) {
+          if (options[i].selected) {
+            value.push(options[i].value);
+          }
+        }
+        this.setState({
+          name: value,
+        });
+      };
+
     
     
-      addNewGame = (event, id) => {
+      addNewEvent = (event, id) => {
         this.props.dispatch({ type: 'ADD_EVENT', payload: this.state });    
         this.setState({
             title:'',
@@ -86,7 +158,7 @@ class AddEvent extends Component {
 
         return (
             <div>
-            <form className = {classes.container} noValidate autoComplete = "off" onSubmit={this.addNewGame}>
+            <form className = {classes.container} noValidate autoComplete = "off" onSubmit={this.addNewEvent}>
                 <div>
                 {/* {JSON.stringify(this.state.props)} */}
                     <TextField
@@ -146,7 +218,45 @@ class AddEvent extends Component {
                             ),
                         }}
                     />
-                    
+                    <FormControl className={classes.multipleRoot}>
+                        <InputLabel htmlFor="select-multiple">Name</InputLabel>
+                        <Select
+                        multiple
+                        value={this.state.name} //**** this will change to this.props.user.username **** //
+                        onChange={this.handleChange}
+                        input={<Input id="select-multiple" />}
+                        MenuProps={MenuProps}
+                        >
+                        {names.map(name => (
+                            <MenuItem key={name} value={name} style={getStyles(name, this)}>
+                                {name}
+                            </MenuItem>
+                        ))}    
+                        </Select>
+                    </FormControl>
+                    <FormControl className={classes.formControl}>
+                        <InputLabel htmlFor="select-multiple-names">Selected</InputLabel>
+                        <Select
+                            multiple
+                            value={this.state.name}
+                            onChange={this.handleChange}
+                            input={<Input id="select-multiple-names" />}
+                            renderValue={selected => (
+                            <div className={classes.chips}>
+                                {selected.map(value => (
+                                <Chip key={value} label={value} className={classes.chip} />
+                                ))}
+                            </div>
+                            )}
+                            MenuProps={MenuProps}
+                        >
+                            {names.map(name => (
+                            <MenuItem key={name} value={name} style={getStyles(name, this)}>
+                                {name}
+                            </MenuItem>
+                            ))}
+                        </Select>
+                        </FormControl>
                     <Button 
                         className={classNames(classes.root, className)} type="submit" >
                         {children || 'Add to Schedule'}
@@ -168,4 +278,4 @@ AddEvent.propTypes = {
     className: PropTypes.string,
 };
 
-  export default connect()(withStyles(styles)(AddEvent));
+  export default connect()(withStyles(styles, { withTheme: true} )(AddEvent));
